5b9ae7c7d274e61fc332bd26eaf14baecf9147e1
Bruno <brunomorello7@gmail.com>
2021-09-20 16:13:26 -0300

    feat: Implementing a GreetingServer

---
+++ src/main/java/br/com/bmo/grpctaskmanager/server/GreetingServer.java
@@ -0,0 +1,24 @@
+package br.com.bmo.grpctaskmanager.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello gRPC");
+
+        Server server = ServerBuilder.forPort(50051).build();
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Received shutdown request");
+            server.shutdown();
+            System.out.println("Successfully stopped server");
+        }));
+
+        server.awaitTermination();
+    }
+}
