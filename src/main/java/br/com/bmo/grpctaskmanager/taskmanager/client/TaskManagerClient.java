d5cf39f1558bb58cdd41b5ed6e035971a93a06bf
Bruno <brunomorello7@gmail.com>
2021-11-13 10:25:58 -0300

    feat: Client to create Tasks

--- src/main/java/br/com/bmo/grpctaskmanager/taskmanager/client/TaskManagerClient.java
+++ src/main/java/br/com/bmo/grpctaskmanager/taskmanager/client/TaskManagerClient.java
@@ -1,5 +1,14 @@
 package br.com.bmo.grpctaskmanager.taskmanager.client;
 
+import br.com.bmo.proto.taskamanager.CreateTaskRequest;
+import br.com.bmo.proto.taskamanager.CreateTaskResponse;
+import br.com.bmo.proto.taskamanager.Task;
+import br.com.bmo.proto.taskamanager.TaskManagerServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import java.time.LocalDateTime;
+
 public class TaskManagerClient {
     public static void main(String[] args) {
         System.out.println("gRPC Client for TaskManager");
@@ -8,5 +17,28 @@ public class TaskManagerClient {
     }
 
     private void run() {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+
+        TaskManagerServiceGrpc.TaskManagerServiceBlockingStub client = TaskManagerServiceGrpc.newBlockingStub(channel);
+
+        Task task = Task.newBuilder()
+                .setDescription("Test Java Client")
+                .setDetails("testing...")
+                .setStatus("CLOSED")
+                .setCreatedAt(LocalDateTime.now().toString())
+                .setUpdatedAt(LocalDateTime.now().toString())
+                .build();
+
+        CreateTaskResponse response = client.createTask(
+                CreateTaskRequest.newBuilder()
+                        .setTask(task)
+                        .build()
+        );
+
+        System.out.println("Received create task response");
+        System.out.println(response.toString());
+
     }
 }
